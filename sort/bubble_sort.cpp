9b185b3cea5e6a99f4cf7ac2386890b1d73320d9
nothingprogram <dev@jongyun.com>
2021-10-31 08:49:26 +0900

    bubble sort

---
+++ sort/bubble_sort.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  int a[101], n, i, j, swap;
+  scanf("%d", &n);
+
+  for (i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
+  }
+
+  for (i = 0; i < n - 1; i++) {
+    for (j = 0; j < n - i - 1; j++) {
+      if (a[j] > a[j + 1]) {
+        swap = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = swap;
+      }
+    }
+  }
+
+  for (i = 0; i < n; i++) {
+    printf("%d ", a[i]);
+  }
+  return 0;
+}
