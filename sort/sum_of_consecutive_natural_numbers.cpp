681a6a82d14124b523b5c55ab654b51e0b7add09
nothingprogram <dev@jongyun.com>
2021-11-06 22:38:07 +0900

    연속된 자연수의 합 (수리)

---
+++ sort/sum_of_consecutive_natural_numbers.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+  int a, b=1, cnt=0, tmp, i;
+  scanf("%d", &a);
+  tmp = a;
+  a--;
+  while (a > 0) {
+    b++;
+    a -= b;
+    if (a % b == 0) {
+      for (i=1; i<b; i++) {
+        printf("%d + ", (a/b)+i);
+      }
+      printf("%d = %d\n", (a/b)+i, tmp);
+      cnt++;
+    }
+  }
+  printf("%d", cnt);
+  return 0;
+}
